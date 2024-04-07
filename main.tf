fbb7b285f5dfd1ba260a3fc178ca62596f0b0b8c
James Glover <aero123uk@gmail.com>
2024-04-08 00:30:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.44.0"
+    }
+  }
+}
+
 provider "aws" {
   profile = "default"
   region = "eu-west-2"
-
+  # Configuration options
 }
+
 
 # Resourses block
 
